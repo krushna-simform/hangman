63fe5099edd3b72907011c09b9314f98a83812f0
krushna rathod <krushna.rathod@simformsolutions.com>
2025-04-30 15:11:34 +0530

    FEATURE: add required data for application

---
+++ src/scripts/model/word.ts
@@ -0,0 +1,40 @@
+const words = [
+    {
+        word: 'marie curie',
+        clue: 'French-Polish scientist who used to carry radium in her pocket',
+    },
+    {
+        word: 'nasa',
+        clue: 'American space program, abbr.',
+    },
+    {
+        word: 'braille',
+        clue: 'A way for the visually impaired to read',
+    },
+    {
+        word: 'media',
+        clue: "Marshall McLuhan's main field of study",
+    },
+    {
+        word: 'tele',
+        clue: 'Prefix for vision or phone',
+    },
+    {
+        word: 'computer',
+        clue: 'What once filled an entire room now fits in our pockets',
+    },
+    {
+        word: 'matter',
+        clue: 'Dark ______, substance which occupies most of [9E-D]',
+    },
+    {
+        word: 'neutral',
+        clue: 'State of an atom with an equal number of electrons and protons',
+    },
+    {
+        word: 'sms',
+        clue: 'Phone message, abbr.',
+    },
+];
+
+export default words;
