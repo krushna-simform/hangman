da0ec83f277060aecf7718a6c6209972f0dfbfbd
krushna rathod <krushna.rathod@simformsolutions.com>
2025-04-30 17:43:29 +0530

    FEATURE: create hangman template for animation

--- src/scripts/view/template.ts
+++ src/scripts/view/template.ts
@@ -29,19 +29,88 @@ const template = {
         const status = document.createElement('div');
         status.setAttribute('id', 'status');
 
+        const button = document.createElement('button');
+        button.setAttribute('id', 'new-game-button');
+        button.innerText = 'new game';
+
         mainRoot.appendChild(hangmanGraphic);
         mainRoot.appendChild(clueSection);
         mainRoot.appendChild(wordDisplayed);
         mainRoot.appendChild(keyboard);
         mainRoot.appendChild(status);
+        mainRoot.appendChild(button);
 
         return mainRoot;
+    },
+
+    hangManGraphics: () => {
+        const hangmanContainer = document.createElement('div');
+        hangmanContainer.setAttribute('id', 'hangman-container');
+
+        const hang = document.createElement('div');
+        hang.setAttribute('id', 'hang');
+
+        const figure = document.createElement('div');
+        figure.setAttribute('id', 'figure');
+
+        const base = document.createElement('div');
+        base.className = 'base';
+
+        const pole = document.createElement('div');
+        pole.className = 'pole';
+
+        const beam = document.createElement('div');
+        beam.className = 'beam';
+
+        const rope = document.createElement('div');
+        rope.className = 'rope';
+
+        hang.appendChild(base);
+        hang.appendChild(pole);
+        hang.appendChild(beam);
+        hang.appendChild(rope);
+
+        const head = document.createElement('div');
+        head.className = 'head part';
+
+        const body = document.createElement('div');
+        body.className = 'body part';
+
+        const leftArm = document.createElement('div');
+        leftArm.className = 'left-arm part';
+
+        const rightArm = document.createElement('div');
+        rightArm.className = 'right-arm part';
+
+        const leftLeg = document.createElement('div');
+        leftLeg.className = 'left-leg part';
+
+        const rightLeg = document.createElement('div');
+        rightLeg.className = 'right-leg part';
+
+        figure.appendChild(head);
+        figure.appendChild(body);
+        figure.appendChild(leftArm);
+        figure.appendChild(rightArm);
+        figure.appendChild(leftLeg);
+        figure.appendChild(rightLeg);
+
+        hangmanContainer.appendChild(hang);
+        hangmanContainer.appendChild(figure);
+
+        return hangmanContainer;
     },
 
     render: () => {
         const main = document.createElement('div');
         main.appendChild(template.header());
-        main.appendChild(template.gauessSection());
+
+        const section = document.createElement('section');
+        section.className = 'hangman-grapic-play';
+        section.appendChild(template.gauessSection());
+        section.appendChild(template.hangManGraphics());
+
+        main.appendChild(section);
         return main;
     },
 };
